83af79dc416bd89cf4fcf01211a128281fb1b347
Adrian Saiz Ferri <asf@click-solutions.de>
2017-12-30 21:16:03 +0100

    responsive

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     $('input').val('');
 });
+
+$('.js-toggle-form').click(function() {
+    $('.my-form').toggleClass('is-hidden');
+    $('.js-toggle-form').toggleClass('is-fixed-bottom');
+});
