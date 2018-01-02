cbf79757a99fe6035d6f1b42e5739d6cd1825b55
Adrian Saiz Ferri <asf@click-solutions.de>
2018-01-02 21:07:15 +0100

    change: form to modal

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
     $('input').val('');
-
-    $('.js-toggle-form span').on('click', function() {
-        $('.my-form').addClass('is-hidden');
-    });
-
-    $('.my-add-btn').on('click', function() {
-        $('.my-form').removeClass('is-hidden');
-    });
-
 });
